d4ecef6c4ac8c524e4d4b7d01126d2d06bca3c0b
svitkaulia <110982580+svitkaulia@users.noreply.github.com>
2024-04-11 12:47:46 +0200

    part3.Continue

--- js/script.js
+++ js/script.js
@@ -4,47 +4,91 @@
 'Сколько фильмов вы уже посмотрели?'
 
 2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+	- count - сюда передается ответ на первый вопрос
+	- movies - в это свойство поместить пустой объект
+	- actors - тоже поместить пустой объект
+	- genres - сюда поместить пустой массив
+	- privat - в это свойство поместить boolean(логическое) значение false
 
 3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
+	- 'Один из последних просмотренных фильмов?'
+	- 'На сколько оцените его?'
 Ответы стоит поместить в отдельные переменные
 Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+	movies: {
+		'logan': '8.1'
+	}
 
 Проверить, чтобы все работало без ошибок в консоли */
 
 'use strict';
 
-let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let numberOfFilms;
+
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+	while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+	}
+}
+
+start();
 
 let personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false,
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt('Один из последних просмотренных фильмов?', '');
-    let b = prompt('На сколько оцените его?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log("Done!)");
-    } else {
-        console.log("Error");
-        i--;
-    } 
+function rememberMyFilms() {
+	for (let i = 0; i < 2; i++) {
+		let a = prompt('Один из последних просмотренных фильмов?', '');
+		let b = prompt('На сколько оцените его?', '');
+	
+		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log("Done!)");
+		} else {
+			console.log("Error");
+			i--;
+		} 
+	}
 }
+// rememberMyFilms();
+
+function detectPersonalLevel() {
+	if (personalMovieDB.count < 10) {
+		console.log("Просмотрено довольно мало фильмов");
+	} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+		console.log("Вы классический зритель");
+	} else if (personalMovieDB.count >= 30) {
+		console.log("Вы киноман");
+	} else {
+		console.log("Произошла ошибка");
+	}
+}
+// detectPersonalLevel();
+
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+	for (let i = 1; i <= 3; i++) {
+		personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+	}
+}
+writeYourGenres();
+
+// console.log(personalMovieDB);
+
+
 
 // let i = 0;
 // while (i < 2) {
@@ -74,17 +118,7 @@ for (let i = 0; i < 2; i++) {
 //     }
 // } while (i < 2);
 
-if (personalMovieDB.count < 10) {
-    console.log("Просмотрено довольно мало фильмов");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("Вы классический зритель");
-} else if (personalMovieDB.count >= 30) {
-    console.log("Вы киноман");
-} else {
-    console.log("Произошла ошибка");
-}
 
-console.log(personalMovieDB);
 
 
 /* Задание на урок:
@@ -101,3 +135,17 @@ str.length - и получить её длину)
 "Вы киноман". А если не подошло ни к одному варианту - "Произошла ошибка"
 
 4) Потренироваться и переписать цикл еще двумя способами*/
+
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
